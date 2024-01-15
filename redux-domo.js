684a93abeb7d991c7919d573a0bd181b5c7a9077
Raushan <roushankumarsingh2018@gmail.com>
2024-01-15 13:05:29 +0530

    decrement counter

---
+++ redux-domo.js
@@ -0,0 +1,36 @@
+// step 1
+const redux = require("redux"); //default node.js import syntax for importing the 3rd party syntax
+
+// step 3
+// reducer function: inputs old state +  dispatched action and returns a output new state object, and reducer fun should be pure function
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+      };
+    default: {
+      return state;
+    }
+  }
+};
+
+// step 2
+// const store = redux.createStore(); //it create the store
+
+const store = redux.createStore(counterReducer); //now someone that subscribe to the reducer
+
+const counterSubscriber = () => {
+  const latestState = store.getState(); // getstate is a method which is availableon the store created with create store
+  console.log("latestState ", latestState);
+};
+store.subscribe(counterSubscriber); //expects a fun when the data in store change
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "increment" });
+
+store.dispatch({ type: "decrement" });
+store.dispatch({ type: "decrement" });
